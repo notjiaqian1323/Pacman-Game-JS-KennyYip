cdbc014bd0033d93e1a79f41b44ff9d3fe5d4e5c
notjiaqian1323 <q12w15e2003@gmail.com>
2025-10-29 17:21:18 +0800

    current progress 20%

--- pacman.js
+++ pacman.js
@@ -64,4 +64,51 @@ function loadImage(){
 // the point of drawing the tile start from the top left corner coord of the tile
 // lets say we want to draw a 32 x 32 tile at point (4,3)
 // then we need to draw (4,3) to (4*32, 3*32) = (128,96)
+// the tiles are added in a 2d array manner: [0][1] means (0,1), [0][2] means (0,2)
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX"
+]
 
+//since we have lot of walls img in the game, we need a data structure to manage it
+const walls = new Set();
+//a set can only have unique values so no duplicates is accepted
+const foods = new Set();
+const ghosts = new Set();
+let pacman;
+
+
+//creating a class for each tile
+class Block {
+    constructor(image, x, y, width, height){
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        //since x and y coord for ghosts and pacman keep changing, so a start position is needed
+        this.startX = x;
+        this.startY = y;
+
+    }
+}
